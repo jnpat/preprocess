22ad2e12e7a4860be366c016980b268cb0166e31
Jnpat <admin@192.168.0.29>
2021-01-22 22:39:01 +0700

    improve

--- Indicator.py
+++ Indicator.py
@@ -21,28 +21,27 @@ class Indicator():
 
   def MA(self,df,n):
     df['MA'] = df['Close'].rolling(n).mean() 
-   
     return df
   
-  def MACD(self,df):#trend 
+  def MACD(self,df,n1,n2,n3,no):#trend 
     '''
     MACD range(-inf,inf)
     '''
     df_MACD = df.copy()
-    df_MACD['EWMA-5'] = df_MACD['Close'].ewm(span=5).mean()
-    df_MACD['EWMA-12'] = df_MACD['Close'].ewm(span=12).mean()
-    df_MACD['EWMA-26'] = df_MACD['Close'].ewm(span=26).mean()
-    df['MACD'] = df_MACD['EWMA-12'] - df_MACD['EWMA-26']
-    df['SIGNAL LINE'] = df['MACD'].ewm(span=9).mean() 
+    # df_MACD['EWMA-5'] = df_MACD['Close'].ewm(span=5).mean()
+    df_MACD['EWMA-12'] = df_MACD['Close'].ewm(span=n1).mean()
+    df_MACD['EWMA-26'] = df_MACD['Close'].ewm(span=n2).mean()
+    df['MACD-'+str(no)] = df_MACD['EWMA-12'] - df_MACD['EWMA-26']
+    df['SIGNAL LINE'+str(no)] = df['MACD-'+str(no)].ewm(span=n3).mean() 
 
     # df['MACD'] = np.where((df_MACD['MACD']>df_MACD['SIGNAL LINE'])&(df_MACD['MACD'].shift()<df_MACD['SIGNAL LINE'].shift()),'buy',
     #                           (np.where((df_MACD['MACD'] < df_MACD['SIGNAL LINE'])  & (df_MACD['MACD'].shift()>df_MACD['SIGNAL LINE'].shift()),'sell','wait or hold')))
 
     return df
 
-  def DMI(self,df):#trend
+  def DMI(self,df,n):#trend
     df_DMI = df.copy()
-    adxi = ADXIndicator(df['High'],df['Low'],df['Close'],14,False)
+    adxi = ADXIndicator(df['High'],df['Low'],df['Close'],n,False)
     df['plusDI'] = adxi.adx_pos()
     df['minusDI'] = adxi.adx_neg()
     df['adx'] = adxi.adx()
@@ -52,20 +51,20 @@ class Indicator():
     
     return df
   
-  # def STOCHRSI(self,df):#momentum
-  #   '''
-  #   data range (0,100)
-  #   '''
-  #   df_STRSI = df.copy()
-  #   strsi = StochRSIIndicator(df['Close'],d1=3,d2=3)
-  #   df['storsi'] = strsi.stochrsi()
-  #   df['%K'] = strsi.stochrsi_k()
-  #   df['%D'] = df['%K'].ewm(span=3).mean()
+  def STOCHRSI(self,df):#momentum
+    '''
+    data range (0,100)
+    '''
+    df_STRSI = df.copy()
+    strsi = StochRSIIndicator(df['Close'])
+    df['storsi'] = strsi.stochrsi()
+    df['%K'] = strsi.stochrsi_k()
+    df['%D'] = df['%K'].ewm(span=3).mean()
 
-  #   # df['STOCHRSI']  = np.where((df_STRSI['%K']>df_STRSI['%D'])&(df_STRSI['%K'].shift()<df_STRSI['%D'].shift())&(df_STRSI['%K']<20),'buy',
-  #   #                             (np.where((df_STRSI['%K'] < df_STRSI['%D']) & (df_STRSI['%K'].shift()>df_STRSI['%D'].shift()) & (df_STRSI['%K']>80),'sell','wait or hold')))
+    # df['STOCHRSI']  = np.where((df_STRSI['%K']>df_STRSI['%D'])&(df_STRSI['%K'].shift()<df_STRSI['%D'].shift())&(df_STRSI['%K']<20),'buy',
+    #                             (np.where((df_STRSI['%K'] < df_STRSI['%D']) & (df_STRSI['%K'].shift()>df_STRSI['%D'].shift()) & (df_STRSI['%K']>80),'sell','wait or hold')))
 
-  #   return df
+    return df
 
   def RSI(self,df):
     def computeRSI (data, time_window):
@@ -81,7 +80,7 @@ class Indicator():
       return rsi
     df['RSI-7'] = computeRSI(df['Close'],7)
     df['RSI-14'] = computeRSI(df['Close'], 14)
-    # df['RSI'] = computeRSI(df['Close'],14)
+    df['RSI'] = computeRSI(df['Close'],14)
     return df
 
   def STOCHASTIC(self,df):
@@ -91,7 +90,7 @@ class Indicator():
     df['%D'] = stoch.stoch_signal()
     return df
 
-  def WILLIAMSR(self,df):
+  def WILLIANSR(self,df):
     williamsr = WilliamsRIndicator(df['High'],df['Low'],df['Close'])
     df['%R'] = williamsr.williams_r()
     return df
@@ -139,12 +138,55 @@ class Indicator():
     df['OBV'] = obv.on_balance_volume()
     return df
 
-  # def AO(self,df):
-  #   '''
-  #   data range(-inf,inf)
-  #   '''
-  #   df_AO = df.copy()
-  #   AO = AwesomeOscillatorIndicator(df['High'],df['Low'])
-  #   df['AO'] = AO.ao()
-  #   return df
+  def AO(self,df):
+    '''
+    data range(-inf,inf)
+    '''
+    df_AO = df.copy()
+    AweOs = AwesomeOscillatorIndicator(df['High'],df['Low'])
+    df['AO'] = AweOs.awesome_oscillator()
+    return df
+
+  def check_trend(self,df):
+    df.loc[(df['Close'] > df['MA']) &
+    ((df['Close'] > df['Close'].shift(1)) | (df['MA'] > df['MA'].shift(1))) &
+    ((df['Close'] > df['Close'].shift(2)) | (df['MA'] > df['MA'].shift(2))) &
+    ((df['Close'] > df['Close'].shift(3)) | (df['MA'] > df['MA'].shift(3))) &
+    ((df['Close'] > df['Close'].shift(4)) | (df['MA'] > df['MA'].shift(4))) &
+    ((df['Close'] > df['Close'].shift(5)) | (df['MA'] > df['MA'].shift(5))) , 'Trend'] = 'Up'
+
+    df.loc[(df['Close'] < df['MA']) &
+    ((df['Close'] < df['Close'].shift(1)) | (df['MA'] < df['MA'].shift(1))) &
+    ((df['Close'] < df['Close'].shift(2)) | (df['MA'] < df['MA'].shift(2))) &
+    ((df['Close'] < df['Close'].shift(3)) | (df['MA'] < df['MA'].shift(3))) &
+    ((df['Close'] < df['Close'].shift(4)) | (df['MA'] < df['MA'].shift(4))) &
+    ((df['Close'] < df['Close'].shift(5)) | (df['MA'] < df['MA'].shift(5))) , 'Trend'] = 'Down'
+    
+    df.loc[df['Trend'].isnull(), 'Ntrend'] = df['Trend'].shift(1)
+    df.loc[df['Trend'].isnull() & df['Trend'].shift(1).isnull(),'Ntrend'] = df['Trend'].shift(2)
+    df.loc[df['Trend'].isnull() & df['Trend'].shift(1).isnull() & df['Trend'].shift(2).isnull(),'Ntrend'] = df['Trend'].shift(3)
+    df.loc[df['Trend'].isnull() & df['Trend'].shift(1).isnull() & df['Trend'].shift(2).isnull() & 
+    df['Trend'].shift(3).isnull(),'Ntrend'] = df['Trend'].shift(4)
+    df.loc[df['Trend'].isnull() & df['Trend'].shift(1).isnull() & df['Trend'].shift(2).isnull() & 
+    df['Trend'].shift(3).isnull() & df['Trend'].shift(4).isnull(),'Ntrend'] = df['Trend'].shift(5)
+    df.loc[df['Trend'].isnull() & df['Trend'].shift(1).isnull() & df['Trend'].shift(2).isnull() & 
+    df['Trend'].shift(3).isnull() & df['Trend'].shift(4).isnull() & df['Trend'].shift(5).isnull(),'Ntrend'] = df['Trend'].shift(6)
+    
+    df['S1'] = df['Close'].shift(-1)
+    df['S2'] = df['Close'].shift(-2)
+    df.loc[df['S1'].isnull(),'S1'] = 0
+    df.loc[df['S2'].isnull(),'S2'] = 0
+
+   
+    #Trading Signal (0,1)
+    df.loc[(df['Trend'] == 'Up') | (df['Ntrend'] == 'Up'), 'Trading Signal'] = ((abs(df['Close'] - df[['Close','S1','S2']].min(axis = 1)) / 
+    abs(df[['Close','S1','S2']].max(axis = 1) - df[['Close','S1','S2']].min(axis = 1)))*0.5) + 0.5
+    df.loc[(df['Trend'] == 'Down') | (df['Ntrend'] == 'Down'), 'Trading Signal'] = ((abs(df['Close'] - df[['Close','S1','S2']].min(axis = 1)) / 
+    abs(df[['Close','S1','S2']].max(axis = 1) - df[['Close','S1','S2']].min(axis = 1)))*0.5)
+   
+    df.loc[df['Trend'].isnull(),'Trend'] =df['Ntrend']
+    
+    df = df.drop(['Ntrend','S1','S2'],axis=1)
+        
+    return df
 
